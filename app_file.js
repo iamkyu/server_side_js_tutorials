a3c76f2177b8d0977ea62f58e46c7dfead70f1d9
kjnam <kyujnam@gmail.com>
2016-04-13 11:42:54 +0900

    [ADD] Databse 연동, Insert, Select 구현

--- app_file.js
+++ app_file.js
@@ -1,5 +1,10 @@
+// express 모듈
 var express = require('express');
+
+// POST 요청 처리를 위한 body-parser 미들웨어
 var bodyParser = require('body-parser');
+
+// multipart/form-data 처리를 위한 모듈
 var multer = require('multer');
 var _storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -10,54 +15,81 @@ var _storage = multer.diskStorage({
   }
 })
 var upload = multer({ storage: _storage })
+
+// file system 관련 모듈
 var fs = require('fs');
+
+// mysql connect 모듈
+var mysql      = require('mysql');
+var conn = mysql.createConnection({
+  host     : '192.168.56.101', // Test DB on My Virtual Machine
+  user     : 'testuser',
+  password : 'java41',
+  database : 'testdb'
+});
+conn.connect;
+
+// app 기본 세팅
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
-app.set('views', './views_file');
+app.set('views', './views_mysql');
 app.set('view engine', 'jade');
 
+
+// 라우트--------------------------------------------------------
 app.get('/upload', function(req, res){
   res.render('upload');
 });
-
 app.post('/upload', upload.single('userfile'), function(req, res){
   res.send('Uploaded : '+req.file.filename);
 });
 
-app.get('/topic/new', function(req, res){
-  fs.readdir('data', function(err, files){
-    if(err){
+app.get('/topic/add', function(req, res){
+  var sql = 'SELECT id, title FROM topic';
+  conn.query(sql, function(err, topics, filed) {
+    if(err) {
       console.log(err);
-      res.status(500).send('Internal Server Error');
+          res.status(500).send('Internal Server Error');
     }
-    res.render('new', {topics:files});
+    res.render('add', {topics:topics});
   });
 });
-
-app.get(['/topic', '/topic/:id'], function(req, res){
-  fs.readdir('data', function(err, files){
-    if(err){
+app.post('/topic/add', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+  var sql = 'INSERT INTO topic(title, description, author) VALUES(?, ?,?)';
+  conn.query(sql, [title, description, author], function(err, result  , fields) {
+    if(err) {
       console.log(err);
       res.status(500).send('Internal Server Error');
+    } else {
+      res.redirect('/topic/'+result.insertId);
     }
+  });
+})
+
+
+app.get(['/topic', '/topic/:id'], function(req, res){
+  var sql = 'SELECT id, title FROM topic';
+  conn.query(sql, function(err, topics, fields) {
     var id = req.params.id;
-    if(id){
-      // id값이 있을 때
-      fs.readFile('data/'+id, 'utf8', function(err, data){
-        if(err){
+    if(id) {
+      var sql = 'SELECT * FROM topic WHERE id = ?';
+      conn.query(sql, [id], function(err, topic, fields) {
+        if(err) {
           console.log(err);
           res.status(500).send('Internal Server Error');
+        } else {
+          res.render('view', {topics:topics, topic:topic[0]});  
         }
-        res.render('view', {topics:files, title:id, description:data});
-      })
+      });
     } else {
-      // id 값이 없을 때
-      res.render('view', {topics:files, title:'Welcome', description:'Hello, JavaScript for server.'});
+      res.render('view', {topics:topics});  
     }
-  })
+  });
 });
-
 app.post('/topic', function(req, res){
   var title = req.body.title;
   var description = req.body.description;
---
+++ sql_query.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `topic` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `author` varchar(30) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--Sample Data
+INSERT INTO topic (title, description, author) VALUES('JavaScript','Computer language for web.', 'egoing');
+INSERT INTO topic (title, description, author) VALUES('NPM','Package manager', 'leezche');
+
